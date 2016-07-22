8bb04b8af061e1b2b0a04519545e0885fbdb4a49
김건호 <geono-kim@baikal.io>
2016-07-22 00:01:51 -0700

    SAVE #26: Add and subtract

--- calc.py
+++ calc.py
@@ -1,14 +1,20 @@
-def calc():
-	# Write code main function
+def calc(): 
+    while 1:
+        num1, num2, func = input("Input Command (ex:1,2,'+') : ") 
+        if (func == '+') : 
+            ret = add(num1, num2) 
+        elif (func == '-') : 
+            ret = sub(num1, num2) 
+        else : 
+            print('Wrong Command!') 
+            ret = None 
+
+ret = calc()
+if (ret != None):
+    print('Result : %d' %ret)
 
 def add(num1, num2):
-	# Write code add function
+    return num1 + num2
 
-def sub(num1, num2):
-	# Write code sub function
-
-def mul(num1, num2):
-	# Write code mul function
-
-def div(num1, num2):
-	# Write code div function
+def sub(num1, num2): 
+    return num1 - num2
