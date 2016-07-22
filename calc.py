290e12a8b61fca512d3df6a0cfe6d4b190fd5756
hesslee <hess.lee70@gmail.com>
2016-07-22 16:17:04 +0900

    [#30] edit sub function. consider divide with 0

--- calc.py
+++ calc.py
@@ -11,4 +11,8 @@ def mul(num1, num2):
 	# Write code mul function
 
 def div(num1, num2):
-	return num1 / num2
+	if num2 :
+            return num1 / num2
+        else :
+            print("You can't divide with 0!")
+            return None
