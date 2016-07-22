c3fab9f4840453ec88521a446e1c773e2117b71f
Chanhyun Park <parkch0708@hanmail.net>
2016-07-22 15:34:49 +0900

    [#25] check out source code file.

---
+++ calc.py
@@ -0,0 +1,14 @@
+def calc():
+	# Write code main function
+
+def add(num1, num2):
+	# Write code add function
+
+def sub(num1, num2):
+	# Write code sub function
+
+def mul(num1, num2):
+	# Write code mul function
+
+def div(num1, num2):
+	# Write code div function
