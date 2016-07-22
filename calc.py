b44c74c89b0f86ae0934eadb4db4091c51095fe4
Chanhyun Park <parkch0708@hanmail.net>
2016-07-22 16:12:04 +0900

    [#25] change main function.

--- calc.py
+++ calc.py
@@ -1,20 +1,24 @@
 def calc(): 
-    while 1:
-        num1, num2, func = input("Input Command (ex:1,2,'+') : ") 
-        if (func == '+') : 
-            ret = add(num1, num2) 
-        elif (func == '-') : 
-            ret = sub(num1, num2) 
-        else : 
-            print('Wrong Command!') 
-            ret = None 
-
-ret = calc()
-if (ret != None):
-    print('Result : %d' %ret)
+    while 1 :
+		num1, num2, func = input("Input Command (ex:1,2,'+') : ")
+		if (func == '+') :
+			ret = add(num1, num2)
+		elif (func == '-') :
+			ret = sub(num1, num2)
+		elif (func == '*') :
+			ret = mul(num1, num2)
+		elif (func == '/') :
+			ret = div(num1, num2)
+		else :
+			print('Wrong Command!')
+			ret = None
+		if (ret != None):
+			print('Result : %d' %ret)
 
 def add(num1, num2):
     return num1 + num2
 
 def sub(num1, num2): 
     return num1 - num2
+
+calc()
